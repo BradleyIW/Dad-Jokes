497368c8062cc6770a959a6a0ae05627d9c90946
BradleyWire <bradley.wilson@wire.com>
2020-03-21 12:52:36 +0100

    Initial commit with generated files (renamed MainActivity to HomeActivity)

---
+++ app/src/main/java/com/bradley/wilson/jokes/home/HomeActivity.kt
@@ -0,0 +1,8 @@
+package com.bradley.wilson.jokes.home
+
+import androidx.appcompat.app.AppCompatActivity
+import com.bradley.wilson.jokes.R
+
+class HomeActivity : AppCompatActivity(R.layout.activity_home) {
+
+}
